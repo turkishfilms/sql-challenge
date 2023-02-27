f6e216d4523084a6e53d60ddd3502a652fa18a90
Reginald Robinson <ddrbnsn@gmail.com>
2023-02-27 14:06:47 -0800

    loop completed

--- tableScheme.sql
+++ tableScheme.sql
@@ -1,15 +1,15 @@
 
 
 CREATE TABLE Depts (
-    dept_no INTEGER NOT NULL;
+    dept_no VARCHAR NOT NULL;
     dept_name VARCHAR NOT NULL;
     PRIMARY KEY(dept_no)
 )
 
 CREATE TABLE Employees(
     emp_no INTEGER NOT NULL;
-    emp_title_id INTEGER NOT NULL;
-    birth_date VARCHAR NOT NULL;
+    emp_title_id VARCHAR NOT NULL;
+    birth_date DATE NOT NULL;
     first_name VARCHAR NOT NULL;
     last_name VARCHAR NOT NULL;
     sex VARCHAR NOT NULL;
